d8037f20fc222d3a2f9d821b6f4a1c8a23277a7e
leshka290 <lleesshhkkaa00@gmail.com>
2023-05-05 11:29:42 +0300

    Решение ДЗ 19

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,45 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
+    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    static   List<String> strings = new ArrayList<>(List.of(
+          "один", "один", "два","два", "два", "три", "три", "три"));
+
+  public static void main(String[] args) {
+        printOddNums();
+        System.out.println();
+        printOddUniqueNums();
+        printUniqueStr();
+        printAmountRepeatStr();
+    }
+
+    public static void printOddNums() {
+        for(Integer i : nums) {
+            if(i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void printOddUniqueNums() {
+        Set<Integer> setNums = new TreeSet<>();
+        for(Integer i : nums) {
+            if(i % 2 == 0) {
+                setNums.add(i);
+            }
+        }
+        System.out.println(setNums);
+    }
+
+    public static void printUniqueStr() {
+        Set<String> setStr = new HashSet<>(strings);
+        System.out.println(setStr);
+    }
+
+    public static void printAmountRepeatStr() {
+        Map<String, Integer> mapStr = strings.stream()
+                .collect(Collectors.toMap(e -> e, e -> 1, Integer::sum));
+        mapStr.forEach((k, v) -> System.out.println(v - 1));
     }
 }
